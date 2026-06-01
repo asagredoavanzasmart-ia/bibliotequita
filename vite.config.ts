2f2381ea8ad32fbe2f2e5fd65cd87bbde42e33f4
avanzasmartgrowth <avanzasmartgrowth@gmail.com>
2026-06-01 09:15:04 -0400

    fix: add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/bibliotequita/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
